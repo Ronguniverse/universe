84dab2798174e942c79eda7b1f7ddaacfb55d0a3
universe <rongunverse@qq.com>
2022-07-14 11:20:06 +0800

    add

---
+++ JavaScripts/Btn(universe)/Event.js
@@ -0,0 +1,9 @@
+function log(){
+    window.location.href="universe/universe.html";
+}
+function sup(){
+    window.location.href="universe/发电.html";
+}
+function dir(){
+    window.location.href=goUrl
+}
